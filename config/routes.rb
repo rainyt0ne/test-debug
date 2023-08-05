2f787b05fde2619ffccb965adab78378b7e800bc
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-05 22:44:14 +0000

    favorite_routes_add

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index,:show,:edit,:update]
+
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :favorites, only[:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
