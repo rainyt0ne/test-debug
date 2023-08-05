f41ee8bf8f966c007e512580de993edea443ac69
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-05 22:34:18 +0000

    model_add

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
+  
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length
-  
-  
+  validates :introduction, presence:true
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
