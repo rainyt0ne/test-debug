9bbecca748fa1ff3bf385b2fe50db1f9fbc76c7b
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-06 00:07:24 +0000

    favorite_good_add

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
 
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-
-  resources :users, only: [:index,:show,:edit,:update] do
-    resource :favorites, only[:create, :destroy]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
   end
+
+
+  resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
